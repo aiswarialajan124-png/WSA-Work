98735a3f9012169350884e85967058eda9c2d12d
lajan <aiswarialajan124@gmail.com>
2026-02-15 12:55:10 +0000

    created books.api.py and added code

---
+++ labs/books.api.py
@@ -0,0 +1,39 @@
+import requests
+
+URL = "http://andrewbeatty1.pythonanywhere.com/books"
+
+# Get all books
+def readbooks():
+    response = requests.get(URL)
+    return response.json()
+
+# Get one book by ID
+def readbook(id):
+    getURL = URL +"/" + str(id)
+    response = requests.get(geturl)
+    return response.json()
+
+# Create a new book
+def createbook(book):
+    response = requests.post(URL, json=book)
+    return response.json()
+
+# Update a book
+def updatebook(id, book):
+    puturl = URL + "/" + str(id)
+    response = requests.put(puturl, json=book)
+    return response.json()
+
+# Delete a book
+def deletebook(id):
+    deleteurl = URL + "/" +str(id)
+    response = requests.delete(deleteurl)
+    return response.json()
+
+# Testing code
+if __name__ == "__main__":
+    print("All books: ")
+    print(readbooks())
+
+    print("\nBook with ID 1: ")
+    print(readbook(1))

ba72c1aec532a87a43aed2f70b12d04e35c2eae4
lajan <aiswarialajan124@gmail.com>
2026-02-21 12:06:10 +0000

    Edited lab05.03

--- labs/Lab04/lab05.03-githubbymodule.py
+++ labs/Lab04/lab05.03-githubbymodule.py
@@ -1,12 +1,21 @@
-from github import Github
+from github import Github, Auth
 from config import config as cfg
+import requests
 
 apikey = cfg["githubkey"]
 
-g = Github(apikey)
+auth = Auth.Token(apikey)
+g = Github(auth=auth)
 
 for repo in g.get_user().get_repos():
     print(repo.name)
 
 repo = g.get_repo("aiswarialajan124-png/aprivateone")
 print(repo.clone_url)
+
+fileInfo = repo.get_contents("test.txt")
+print(fileInfo.download_url)
+
+response = requests.get(fileInfo.download_url)
+contentOfFile = response.text
+print(contentOfFile)
